f13986c8728e05c0f74922271112185c260ad87d
damilola <damiloladolor@gmail.com>
2021-09-05 10:39:45 +0100

    removed pyc files from git

--- chef_management/chef_management/settings.py
+++ chef_management/chef_management/settings.py
@@ -41,19 +41,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Application definition
 
 INSTALLED_APPS = [
-<<<<<<< HEAD
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # 3rd party apps
     "ckeditor",
     "crispy_forms",
-    'mathfilters',
-
+    "mathfilters",
     # Local apps
     "chef_management_app",
     "quiz",
